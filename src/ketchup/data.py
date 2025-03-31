8c57b06faad8cc502b584c49cbe924b5d435943a
hnthap <94937751+hnthap@users.noreply.github.com>
2025-03-31 13:47:59 +0700

    fix

--- src/ketchup/data.py
+++ src/ketchup/data.py
@@ -117,29 +117,46 @@ def get_papers(paper_ids: list[str], db_name: str) -> list[Paper]:
         SELECT 
             p.paper_id,
             s.full_name AS submitter,
-            GROUP_CONCAT(a.full_name, ', ' ORDER BY ap.ordering) AS authors,
+            (
+                SELECT GROUP_CONCAT(a2.full_name, ', ')
+                FROM authorship ap2
+                LEFT JOIN person a2 ON ap2.author_id = a2.person_id
+                WHERE ap2.paper_id = p.paper_id
+                ORDER BY ap2.ordering
+            ) AS authors,
             p.title,
             p.journal,
             p.doi,
             p.abstract,
             p.year,
-            GROUP_CONCAT(c.category, ', ') AS categories
+            GROUP_CONCAT(c.name, ', ') AS categories
         FROM paper p
         LEFT JOIN person s ON p.submitter_id = s.person_id
         LEFT JOIN authorship ap ON p.paper_id = ap.paper_id
         LEFT JOIN person a ON ap.author_id = a.person_id
         LEFT JOIN paper_category pc ON p.paper_id = pc.paper_id
         LEFT JOIN category c ON pc.category_id = c.category_id
-        WHERE p.paper_id = ?
+        WHERE p.paper_id IN (%s)
         GROUP BY p.paper_id
-    '''
+    ''' % (', '.join(list(map(lambda s: "'%s'" % s, paper_ids))))
     with sqlite3.connect(db_name) as conn:
         try:
             cursor = conn.cursor()
-            cursor.executemany(query, list(paper_ids))
+            cursor.execute(query)
             rows = cursor.fetchall()
             papers = [
-                Paper(**dict(zip(cursor.column_names, row))) for row in rows
+                Paper(
+                    paper_id=row[0],
+                    submitter=row[1],
+                    authors=row[2].split(', '),
+                    title=row[3],
+                    journal=row[4],
+                    doi=row[5],
+                    abstract=row[6],
+                    year=row[7],
+                    categories=row[8].split(', '),
+                )
+                for row in rows
             ]
             return papers
         except sqlite3.Error as e:
